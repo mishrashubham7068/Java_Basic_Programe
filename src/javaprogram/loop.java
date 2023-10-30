a4843dec5122aef21ce140b315893b615814c4c1
ShubhamMishra <subhammishra660@gmail.com>
2023-10-30 10:41:09 +0530

    Star Pattern

---
+++ src/javaprogram/loop.java
@@ -0,0 +1,15 @@
+package javaprogram;
+
+public class loop {
+    public static void main(String[] args){
+        int row=10;
+        for (int i=0;i<row;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print('x');
+            }
+            System.out.println();
+        }
+
+
+    }
+}
